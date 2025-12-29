bb73e2a67db550d500af018bc5c483ccfe18da21
Cristian Agudelo <cagudelo3245@gmail.com>
2025-12-29 10:00:45 -0500

    fix scroll

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-zinc-950">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1 px-4 sm:px-6 lg:px-8 py-8 max-w-7xl mx-auto w-full pt-20">
         <Outlet />
